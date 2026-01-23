41dc3aad6f60d266113449c96f216b33a35ad9dd
Nicholas Homes <nicholas.lee.h@icloud.com>
2026-01-23 07:58:50 -0500

    Forgot to include some stuff in the previous commit.

--- app/src/main/java/org/example/controllers/ViewController.java
+++ app/src/main/java/org/example/controllers/ViewController.java
@@ -28,7 +28,7 @@ public class ViewController{
     double rayAngle = player.getHeading() - ((Math.PI/3) / 2) + i * angleStep; 
     double[] collision = ViewService.castRay(new Ray(player.getPosX(), player.getPosY(), rayAngle), vModel.getMap());
     double correctedDistance = collision[3] * Math.cos(collision[4] - vModel.getPlayer().getHeading());//Fixing fisheye effect by adjusting distance
-    double wallHeight = (64 * 480) / collision[3];//Wall height = tileSize * screenHeight divided by the corrected distance
+    double wallHeight = (64 * 480) / correctedDistance;//Wall height = tileSize * screenHeight divided by the corrected distance
     double wallTop = (480 - wallHeight) / 2;//Wall top = screenHeight - wallHeight divided by 2. (centers the wall in the view)
     double columnWidth = (double)640 / 60;//Calculating the width of each screen slice
     double x = i * columnWidth;//Calculating the starting x coordinate of the slice corresponding to this ray
